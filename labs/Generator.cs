22e4ef849c641e0b5510f8bdd4331a9638df546a
andreych <cad.dsw.dwa@gmail.com>
2024-02-28 17:18:31 +0500

    Add project files.

---
+++ labs/Generator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    public class Generator
+    {
+        public int[,] MatrixA;
+        public int[,] MatrixB;
+        public int[,] TransposedMatrix;
+
+        public Generator(int n)
+        {
+            int N = n;
+            int[,] matrixA = GenerateRandomMatrix(N);
+            int[,] matrixB = GenerateRandomMatrix(N);
+            
+            SaveMatrixToBinaryFile(matrixA, "matrixA.bin");
+            SaveMatrixToBinaryFile(matrixB, "matrixB.bin");
+
+            Console.WriteLine("Бинарные файлы matrixA.bin и matrixB.bin созданы.");
+
+            MatrixA = matrixA;
+            MatrixB = matrixB;
+        }
+        static int[,] GenerateRandomMatrix(int N)
+        {
+            Random rand = new Random();
+            int[,] matrix = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = rand.Next(1, 10);
+                }
+            }
+            return matrix;
+        }
+
+        static void SaveMatrixToBinaryFile(int[,] matrix, string filePath)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                int rows = matrix.GetLength(0);
+                int cols = matrix.GetLength(1);
+
+                writer.Write(rows);
+                writer.Write(cols);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        writer.Write(matrix[i, j]);
+                    }
+                }
+            }
+        }
+
+        public void CreateTransposeMatrix(int[,] matrix, string name)
+        {
+            int[,] transposedMatrixA = TransposeMatrix(matrix);
+            TransposedMatrix = transposedMatrixA;
+
+            SaveMatrixToBinaryFile(transposedMatrixA, $"transposedMatrix{name}.bin");
+        }
+        static int[,] TransposeMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] transposedMatrix = new int[cols, rows];
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    transposedMatrix[i, j] = matrix[j, i];
+                }
+            }
+
+            return transposedMatrix;
+        }
+
+    }
+}
---
+++ labs/Program.cs
@@ -0,0 +1,104 @@
+﻿using labs;
+using System;
+using System.IO;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main()
+    {
+        var test = 5 * 5;
+
+        Generator generator = new Generator(100);
+
+        generator.CreateTransposeMatrix(generator.MatrixB,"B");
+
+        var timeStar = DateTime.Now;
+        CalculateMatrixMultiplication(100, "matrixA.bin", "transposedMatrixB.bin", "resultRowColumn.bin");
+        var timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения row by column: " + (timeEnd - timeStar).Ticks);
+
+        timeStar = DateTime.Now;
+        generator.CreateTransposeMatrix(generator.TransposedMatrix, "T");
+        CalculateMatrixMultiplication(100, "matrixA.bin", "transposedMatrixT.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения row by column(T): " + (timeEnd - timeStar).Ticks);
+
+        timeStar = DateTime.Now;
+        CalculateMatrixMultiplication(100, "matrixA.bin", "matrixB.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения row by row: " + (timeEnd - timeStar).Ticks);
+
+        timeStar = DateTime.Now;
+        generator.CreateTransposeMatrix(generator.MatrixB, "B");
+        CalculateMatrixMultiplication(100, "matrixA.bin", "transposedMatrixB.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения row by row(T): " + (timeEnd - timeStar).Ticks);
+
+        generator.CreateTransposeMatrix(generator.MatrixA, "A");
+        generator.CreateTransposeMatrix(generator.MatrixB, "B");
+        timeStar = DateTime.Now;
+        CalculateMatrixMultiplication(100, "transposedMatrixA.bin", "transposedMatrixB.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения column by column: " + (timeEnd - timeStar).Ticks);
+
+        timeStar = DateTime.Now;
+        generator.CreateTransposeMatrix(generator.TransposedMatrix, "T");
+        CalculateMatrixMultiplication(100, "transposedMatrixA.bin", "transposedMatrixT.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения column by column(T): " + (timeEnd - timeStar).Ticks);
+
+
+        timeStar = DateTime.Now;
+        CalculateMatrixMultiplication(100, "transposedMatrixA.bin", "matrixB.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения column by row: " + (timeEnd - timeStar).Ticks);
+
+        timeStar = DateTime.Now;
+        generator.CreateTransposeMatrix(generator.MatrixB, "B");
+        CalculateMatrixMultiplication(100, "transposedMatrixA.bin", "transposedMatrixB.bin", "resultRowColumn.bin");
+        timeEnd = DateTime.Now;
+        Console.WriteLine("Время выполнения column by row(T): " + (timeEnd - timeStar).Ticks);
+    }
+
+    static void CalculateMatrixMultiplication(int matrixLength, string matrixAFilePath, string matrixBFilePath, string resultFilePath)
+    {
+        int N = matrixLength;
+
+        using (BinaryReader matrixAReader = new BinaryReader(File.Open(matrixAFilePath, FileMode.Open)))
+        using (BinaryReader matrixBReader = new BinaryReader(File.Open(matrixBFilePath, FileMode.Open)))
+        using (BinaryWriter resultWriter = new BinaryWriter(File.Open(resultFilePath, FileMode.Create)))
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int sum = 0;
+                    matrixAReader.BaseStream.Seek(i * N * sizeof(int), SeekOrigin.Begin);
+                    matrixBReader.BaseStream.Seek(j * sizeof(int), SeekOrigin.Begin);
+                    for (int k = 0; k < N; k++)
+                    {
+                        int a = matrixAReader.ReadInt32();
+                        int b = matrixBReader.ReadInt32();
+                        sum += a * b;
+                    }
+                    resultWriter.Write(sum);
+                }
+            }
+        }
+    }
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
