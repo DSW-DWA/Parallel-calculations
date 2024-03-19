8131fa69de4069fda72369d8537c7d5db25dd798
andreych <cad.dsw.dwa@gmail.com>
2024-03-19 22:52:38 +0500

    fin task 2

--- labs/Program.cs
+++ labs/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 class Program
 {
     static double Function(double x)
     {
-        return 8 + 2*x - (x*x);
+        return Math.Pow(Math.E, x);
     }
 
     static double SequentialIntegration(double a, double b, double epsilon)
@@ -26,10 +24,10 @@ class Program
             }
 
             double currentResult = sum;
-
+            //Console.WriteLine("Result: {0}, Epsilon: {1}, Time: {2}", currentResult, epsilon, Stopwatch.GetTimestamp());
             if (Math.Abs(currentResult - prevResult) < epsilon)
                 break;
-
+            
             prevResult = currentResult;
             /*n *= 2;*/
             h /= 2;
@@ -40,13 +38,12 @@ class Program
         return result;
     }
 
-    static double ParallelIntegration(double a, double b, double epsilon, int numThreads)
+    /*static double ParallelIntegration(double a, double b, double epsilon, int numThreads)
     {
         double result = 0;
         double subStep = (b - a) / numThreads;
         double prevResult = double.MaxValue;
         var step = subStep / 2;
-
         while (true)
         {
             result = 0;
@@ -55,9 +52,9 @@ class Program
             {
                 var threadA = a + i * subStep;
                 var threadB = a + (i + 1) * subStep;
-
                 var th = new Thread(() =>
                 {
+                    var t = 5 * 5;
                     double threadResult = 0;
                     for (var x = threadA; x < threadB; x += step)
                     {
@@ -67,13 +64,70 @@ class Program
                     result += threadResult;
                 });
 
+                listThread.Add(th);
+            }
+            foreach (var th in listThread)
+            {
                 th.Start();
+                th.Join();
+            } 
+                
+            if (Math.Abs(result - prevResult) < epsilon)
+                break;
+
+
+            prevResult = result;
+            step /= 2;
+        }
+
+
+        return result;
+    }*/
+
+    static double ParallelIntegration(double a, double b, double epsilon, int numThreads)
+    {
+        double result = 0;
+        double subStep = (b - a) / numThreads;
+        double prevResult = double.MaxValue;
+        var step = subStep / 2;
+        while (true)
+        {
+            result = 0;
+            var listThread = new List<Thread>();
+            var threadResults = new double[numThreads];
+            for (var i = 0; i < numThreads; i++)
+            {
+                var threadA = a + i * subStep;
+                var threadB = a + (i + 1) * subStep;
+                var th = new Thread((object indexObj) =>
+                {
+                    var t = 5 * 5;
+                    int index = (int)indexObj;
+                    double threadResult = 0;
+                    for (var x = threadA; x < threadB; x += step)
+                    {
+                        threadResult += Function(x) * step;
+                    }
+                    threadResults[index] = threadResult; 
+                });
 
                 listThread.Add(th);
             }
+                
+            for (var i = 0; i < numThreads; i++)
+            {
+                listThread[i].Start(i);
+            }
 
             foreach (var th in listThread)
+            {
                 th.Join();
+            }
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                result += threadResults[i];
+            }
 
             if (Math.Abs(result - prevResult) < epsilon)
                 break;
@@ -81,26 +135,24 @@ class Program
             prevResult = result;
             step /= 2;
         }
-        
 
         return result;
     }
 
-
     static void Main(string[] args)
     {
         var t = 5 * 5;
-        double a = -2;
-        double b = 4;
-        double epsilon = 0.000000001;
-        double starEpsilon = 0.001;
-        int maxThreads = 8;
+        double a = 0;
+        double b = 10;
+        double epsilon = 0.001;
+        double starEpsilon = 0.1;
+        int maxThreads = 10;
 
         Console.WriteLine("Sequential Integration:");
         for (double i = starEpsilon; i >= epsilon; i /=10)
         {
             Stopwatch sw = Stopwatch.StartNew();
-            double result = SequentialIntegration(a, b, epsilon);
+            double result = SequentialIntegration(a, b, i);
             sw.Stop();
             Console.WriteLine($"Epsilon: {i.ToString("F10")}, Result: {result}, Time: {sw.ElapsedMilliseconds} ms");
         }
@@ -109,20 +161,15 @@ class Program
         for (double i = starEpsilon; i >= epsilon; i /=10)
         {
             Console.WriteLine("Epsilon: " + i.ToString("F10"));
-            for (int j = 1; j <= maxThreads; j++)
+            for (int j = 4; j <= maxThreads; j++)
             {
-                long sum = 0;
                 double result = 0;
-                for (var k = 0; k < 10; k ++)
-                {
-                    Stopwatch sw = Stopwatch.StartNew();
-                    result = ParallelIntegration(a, b, i, j);
+                Stopwatch sw = Stopwatch.StartNew();
+                result = ParallelIntegration(a, b, i, j);
 
-                    sw.Stop();
-                    sum += sw.ElapsedMilliseconds;
-                }
+                sw.Stop();
                 
-                Console.WriteLine($"Threads: {j}, Result: {result}, Time: {sum/10} ms");
+                Console.WriteLine($"Threads: {j}, Result: {result}, Time: {sw.ElapsedMilliseconds} ms");
             }
             Console.WriteLine();
         }
